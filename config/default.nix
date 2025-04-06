efe4ee0eac682dddb1b663b16b6ba0365caa98e3
kobipana <kobipana@gmail.com>
2025-04-06 23:04:03 +0200

    add startify

--- config/default.nix
+++ config/default.nix
@@ -33,7 +33,8 @@ _: {
     # UI plugins
     ./plugins/ui/bufferline.nix
     ./plugins/ui/lualine.nix
-    ./plugins/ui/startup.nix
+    # ./plugins/ui/startup.nix
+    ./plugins/ui/startify.nix
 
     # LSP and formatting
     ./plugins/lsp/lsp.nix
